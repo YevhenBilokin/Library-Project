9569a4811f69527684e4a5390ee9a9d2b31768fb
Yevhen Bilokin <zenyen14@gmail.com>
2024-11-12 17:24:38 +0100

    Настройка GUI

--- gui_review.py
+++ gui_review.py
@@ -17,12 +17,15 @@ def loggin():
     else:
         messagebox.showerror("Ошибка", "Логин не был введен!")
 
+#Функция для возвращения к меню авторизации
 def log_out(dashboard):
     dashboard.destroy()
     root.deiconify()
 
 #Открытие окна личного кабинета
 def open_dashboard():
+    global dashboard
+
     dashboard = tk.Toplevel()
     dashboard.title("Личный кабинет")
     dashboard.geometry("600x400")
@@ -39,7 +42,36 @@ def open_dashboard():
     library_info_text = tk.Label(dashboard, text="Библиотека")
     library_info_text.pack(anchor="center", side="top")
     
+    
+    button_frame = tk.Frame(dashboard)
+    button_frame.pack(pady=20)
 
+    borrow_book_button = tk.Button(button_frame, text="Взять книгу", command=open_borrow_book)
+    return_book_button = tk.Button(button_frame, text="Вернуть книгу")
+    borrow_book_list_button = tk.Button(button_frame, text="Список взятых книг")
+
+    borrow_book_button.pack(side="left", padx=10)
+    return_book_button.pack(side="left", padx=10)
+    borrow_book_list_button.pack(side="left", padx=10)
+
+
+def open_borrow_book():
+    dashboard.destroy()
+
+    borrow_book = tk.Toplevel()
+    borrow_book.title("Взять книгу")
+    root.geometry("600x400")
+
+    #Информации о пользователе в правом верхнем углу
+    user_info_text = tk.Label(borrow_book, text=f"Вы вошли под логином: {user_name.name}")
+    user_info_text.pack(anchor="ne", side="top")
+
+    #Кнопка выхода с личного кабинета
+    log_out_button = tk.Button(borrow_book, text="Выход", command=lambda: log_out(borrow_book))
+    log_out_button.pack(anchor="ne", side="top")
+
+    borrow_book_title = tk.Label(borrow_book, text="Выберите книгу которую хотите взять")
+    borrow_book_title.pack(anchor="center", side="top")
 
 
 
